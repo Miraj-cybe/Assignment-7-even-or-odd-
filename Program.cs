ce56bd9b85864c3b554676ec5e824cceb31d59fb
Miraj-cybe <mondolm619@gmail.com>
2025-01-26 10:31:57 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+class Program
+{
+public static void Main()
+{
+
+
+int number;
+Console.Write("Enter a Number for check its even/odd: ");
+ number=Convert.ToInt32(Console.ReadLine());
+
+ if(number %2==0)
+ {
+     Console.WriteLine($"{number}  is even");
+ }
+
+else{
+     Console.WriteLine($"{number}  is odd");
+
+
+
+
+
+
+}
+}
+}
